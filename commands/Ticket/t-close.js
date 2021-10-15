0bcffbb67cc4ea1a74e3260a5b6ce68aeda7d754
DEVIL7777 <78595889+DC-Guru@users.noreply.github.com>
2021-10-15 18:38:57 +0000

    Initial commit

---
+++ commands/Ticket/t-close.js
@@ -0,0 +1,32 @@
+const colors = require('./../../colors.json')
+module.exports = {
+    name: 'tclose',
+    aliases: ['t-close'],
+    description: 'Delete a specified ticket.',
+    usage: '',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async (client, message, args) => {
+
+        if (message.channel.name.includes('ticket-')) {
+            message.channel.delete();
+        }
+        else {
+            return message.reply('❌ you cannot use this command here. Please use this command when you want to delete a ticket.');
+        }
+
+    }
+}
+
+
+/**
+ * @INFO
+ * Bot Coded by RogmitOp#6051 |
+ * https://www.youtube.com/channel/UCPJRB-I9FCkWzgN3c2vKIpQ
+ * @INFO
+ * Please mention Him , when using this Code!
+ * @INFO
+ */
